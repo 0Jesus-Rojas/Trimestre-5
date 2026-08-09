bb69f1cd95c1fbc586150d91255b41e561b0eae3
0Jesus-Rojas <jesusdavid181003@gmail.com>
2026-08-09 15:37:48 -0500

    main

--- guarderiaJdrg/src/java/Controlador/ActividadesDAO.java
+++ guarderiaJdrg/src/java/Controlador/ActividadesDAO.java
@@ -15,50 +15,99 @@ import java.sql.ResultSet;
  * @author Aprendiz
  */
 public class ActividadesDAO {
-    
+
     private Conexion conect = new Conexion();
-    
-    public Actividades consultarActividades(String idActividad){
+
+    public Actividades consultarActividades(String idActividad) {
         Connection conn = conect.getconn();
-        
+
         Actividades miActividad = null;
-        
-        try{
+
+        try {
             String querySql = "Select id_actividades, descripcion_actividad FROM actividades WHERE id_actividades = ?";
-            
+
             PreparedStatement ps = conn.prepareStatement(querySql);
-            
+
             ps.setString(1, idActividad);
-            
+
             ResultSet rs = ps.executeQuery();
-            if(rs.next()){
+            if (rs.next()) {
                 miActividad = new Actividades();
-                
+
                 miActividad.setIdActividades(rs.getInt("id_actividades"));
                 miActividad.setDescripcionActividad(rs.getString("descripcion_actividad"));
             }
-        }catch(SQLException e){
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
             return miActividad;
         }
         return miActividad;
     }
-    public boolean InsertarActividades(Actividades miActividad){
+
+    public boolean InsertarActividades(Actividades miActividad) {
         boolean insertar = false;
         Connection conn = conect.getconn();
-        
-         try{
-             String querySql = "INSERT INTO actividades(descripcion_actividad) VALUES (?)";
-             
-             PreparedStatement ps = conn.prepareStatement(querySql);
-             ps.setString(1, miActividad.getDescripcionActividad());
-             
-             ps.executeUpdate();
-             insertar = true;
-             System.out.println("Actividad registrada");
-         }catch(Exception e){
-             System.out.println("Error al registrar la actividad" + e.getMessage());
-         }
-         return insertar;
+
+        try {
+            String querySql = "INSERT INTO actividades(descripcion_actividad) VALUES (?)";
+
+            PreparedStatement ps = conn.prepareStatement(querySql);
+            ps.setString(1, miActividad.getDescripcionActividad());
+
+            ps.executeUpdate();
+            insertar = true;
+            System.out.println("Actividad registrada");
+        } catch (Exception e) {
+            System.out.println("Error al registrar la actividad" + e.getMessage());
+        }
+        return insertar;
+    }
+
+    public boolean eliminarActividades(int idActividad) {
+        boolean eliminado = false;
+        Connection conn = conect.getconn();
+
+        try {
+            String querySql = "DELETE FROM actividades WHERE id_actividades = ?";
+            PreparedStatement ps = conn.prepareStatement(querySql);
+
+            ps.setInt(1, idActividad);
+
+            int filasAfectadas = ps.executeUpdate();
+
+            if (filasAfectadas > 0) {
+                eliminado = true;
+            } else {
+                System.out.println("No se encontro el ID dado");
+            }
+
+        } catch (SQLException e) {
+            System.out.println("No se pudo eliminar la actividad" + e.getMessage());
+        }
+        return eliminado;
+    }
+
+    public boolean actualizarActividades(int idActividad, String nuevaDescripcion) {
+        boolean actualizado = false;
+        Connection conn = conect.getconn();
+
+        try {
+            String querySql = "UPDATE actividades SET descripcion_actividad = ? WHERE id_actividades = ?";
+            PreparedStatement ps = conn.prepareStatement(querySql);
+            ps.setString(1, nuevaDescripcion);
+            ps.setInt(2, idActividad);
+
+            int filasAfectadas = ps.executeUpdate();
+
+            if (filasAfectadas > 0) {
+                actualizado = true;
+            } else {
+                System.out.println("No se pudo encontrar la actividad");
+            }
+        } catch (SQLException e) {
+            System.out.println("No se pudo editar la actividad " + e.getMessage());
+        }
+        return actualizado;
+
     }
 }
